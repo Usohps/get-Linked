188b88ec238d862ca6e7560be540b822c1e0b464
Usohps <usohps@gmail.com>
2023-10-05 00:37:46 +0100

    contact_modal

--- app/modal/Success.jsx
+++ app/modal/Success.jsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import SuccessIcon from "@/assets/congratulation.png";
-function Success() {
+import { AiOutlineClose } from "react-icons/ai";
+function Success({closeModal}) {
   return (
-    <div className=" w-full h-full md:hidden flex justify-center fixed top-[0] backdrop-blur-sm animation  left-0 from-transparent to-[#253D62]">
+    <div className=" w-full h-full flex justify-center fixed top-[0] backdrop-blur-sm animation  left-0 from-transparent to-[#253D62]">
       <div className="flex flex-col ease-in-out translate-x-10 justify-center items-center m-auto bg-transparent h-[400px] font-bold">
+      <AiOutlineClose size={24} className="text-white" onClick={closeModal} />
         <div className="w-[350px] backdrop-blur-2xl  bg-gradient-to-r from-blue-950 to-purple-900 shadow-slate-900 shadow-2xl p-2 text-white h-[400px] m-auto flex flex-col justify-center items-center text-xl space-y-4 rounded-lg">
           <Image src={SuccessIcon} alt="success_img" className="w-[150px]" />
           <div className="text-sm text-center">
